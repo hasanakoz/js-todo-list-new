c69e820efead4df1932e37238aaa8f4861322922
hasan <hasanakoz1986@gmail.com>
2022-09-19 23:27:37 +0300

    add items to, remove items from local storage

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const addBtn = document.querySelector("#todo-button");
 const todoInput = document.getElementById("todo-input");
 const todoUl = document.getElementById("todo-ul");
 
+let todos = JSON.parse(localStorage.getItem("TODOS")) || [];
+
+const renderSavedTodos = () => {
+  todos.forEach((todo) => {
+    createListElement(todo);
+  });
+};
+renderSavedTodos();
+
 addBtn.addEventListener("click", () => {
   if (todoInput.value.trim() === "") {
     alert("please enter a task");
@@ -16,11 +25,13 @@ addBtn.addEventListener("click", () => {
       text: todoInput.value,
     };
     createListElement(newToDo);
+    todos.push(newToDo);
+    localStorage.setItem("TODOS", JSON.stringify(todos));
     todoInput.value = "";
   }
 });
 
-const createListElement = (newToDo) => {
+function createListElement(newToDo) {
   const li = document.createElement("li");
   li.setAttribute("id", newToDo.id);
 
@@ -39,14 +50,18 @@ const createListElement = (newToDo) => {
   deleteIcon.setAttribute("class", "fas fa-trash");
   li.appendChild(deleteIcon);
   todoUl.appendChild(li);
-};
+}
 
 todoUl.addEventListener("click", (e) => {
+  const id = e.target.parentElement.getAttribute("id");
   if (e.target.classList.contains("fa-trash")) {
     e.target.parentElement.remove();
+    todos = todos.filter((todo) => todo.id != id);
+    localStorage.setItem("TODOS", JSON.stringify(todos));
   }
 
   if (e.target.classList.contains("fa-check")) {
+    e.target.parentElement.classList.toggle("completed");
   }
 });
 
